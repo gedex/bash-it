b0cc1cc4b45016f6b1dd72407748cb435de6fd33
Akeda Bagus <admin@gedex.web.id>
2013-03-11 15:16:34 +0700

    Added support for linux's browser.

--- plugins/available/browser.plugin.bash
+++ plugins/available/browser.plugin.bash
@@ -9,18 +9,46 @@ function browser() {
     example '$ ron -5 man/rip.5.ron | browser'
     group 'browser'
 
-    if [ -t 0 ]; then
-        if [ -n "$1" ]; then
-            open $1
-        else
-            reference browser
-        fi
+    case $OSTYPE in
+        linux*)            
+            if [ -x "$(which google-chrome 2>/dev/null)" ]
+            then
+                AVAILABE_BROWSER="google-chrome"
+            elif [ -x "$(which firefox 2>/dev/null)" ]
+            then
+                AVAILABE_BROWSER="firefox"
+            else
+                printf '%s\n' 'No google-chrome or firefox'
+                exit 1
+            fi
 
-    else
-        f="/tmp/browser.$RANDOM.html"
-        cat /dev/stdin > $f
-        open $f
-    fi
+            if [ -t 0 ]
+            then
+                if [ -n "$1" ]
+                then
+                    $AVAILABE_BROWSER "$1"
+                fi
+            else
+                f="/tmp/browser.$RANDOM.html"
+                cat /dev/stdin > $f
+                $AVAILABE_BROWSER "$f"
+            fi
+            ;;
+        darwin*)
+            if [ -t 0 ]; then
+                if [ -n "$1" ]; then
+                    open $1
+                else
+                    reference browser
+                fi
+
+            else
+                f="/tmp/browser.$RANDOM.html"
+                cat /dev/stdin > $f
+                open $f
+            fi
+            ;;
+    esac
 }
 
 
