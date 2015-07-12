64a81c97f63d2732e71b59c6500491e4732d704f
Akeda Bagus <admin@gedex.web.id>
2015-07-13 00:30:25 +0700

    Added more aliases for akeda.

--- aliases/available/akeda.aliases.bash
+++ aliases/available/akeda.aliases.bash
@@ -15,6 +15,16 @@ then
 fi
 alias stpackages='open ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/'
 
+# Basic commands
+alias l="ls -al"
+alias c="clear"
+alias q="exit"
+alias jsont="python -m json.tool"
+function open_localhost() {
+	open "http://localhost:${1:-80}${2:-/}"
+}
+alias opl=open_localhost
+
 # DNSCrypt
 # Install first via brew
 # brew install dnscrypt-proxy
@@ -37,8 +47,45 @@ fi
 alias phpserver="$PHP_CLI -S localhost:8080" # PHP 5.4
 alias cake='./lib/Cake/Console/cake' # cake 2*
 alias phptidy='/Users/akeda/Works/PHP/wp-phptidy/wp-phptidy.php'
-alias phpcs='/usr/local/php/bin/phpcs' # PHP sniffer
+alias phpcs='php ~/Works/PHP/phpcs/scripts/phpcs' # PHP sniffer
 alias phpa='/usr/local/phpa-norl'
 
 # Github
 alias gmd='~/Works/PHP/preview-github-readme.md/preview_github_readme' # https://github.com/gedex/preview-github-readme.md
+
+# MD5
+alias md5='md5 -r'
+alias md5sum='md5 -r'
+
+# Docker
+alias bd='boot2docker'
+alias bds='boot2docker status'
+alias bdu='boot2docker up'
+alias bdd='boot2docker down'
+alias bdi='$(boot2docker shellinit | grep export)'
+alias bdip='boot2docker ip'
+alias d='docker'
+alias dt='docker --tls'
+alias dtr='docker --tls run'
+alias dtri='docker --tls run -t -i'
+alias dtrd='docker --tls run -d'
+alias dtl='docker --tls ps -l'
+alias dta='docker --tls ps -a'
+alias dtps='docker --tls ps'
+alias dtim='docker --tls images'
+alias dts='docker --tls stop'
+alias dti='docker --tls inspect'
+alias dtrm='docker --tls rm'
+alias dtrmi='docker --tls rmi'
+alias dtb='docker --tls build -t'
+function open_dockerhost() {
+	bdip="$(bd ip)"
+	open "http://${bdip:-localhost}:${1:-80}$2"
+}
+alias odh=open_dockerhost
+
+# docker-compose
+alias dco='docker-compose'
+alias dcps='docker-compose ps'
+alias dcup='docker-compose up'
+alias dcupd='docker-compose up -d'
